7e11f8bfe4335b90e24f8b207ebe4670b621c589
ccumez2020082 <88795566+ccumez2020082@users.noreply.github.com>
2022-07-06 19:59:32 -0600

    proyecto 1 terminado

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function mostrarValores(name) {
+    let letraNumero;
+    let mostrarDatos = ['a', 'b', 'c'];
+    let mostrarDatosNumero = [1, 2, 3];
+    console.log('Letras y Numeros', mostrarDatos, mostrarDatosNumero);
+}
+mostrarValores('Ejecutar funcion.....');
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+export{};
+function mostrarValores(name: string){      
+    type letrasYNumeros  = string|number;
+    let letraNumero: letrasYNumeros;
+    let mostrarDatos: letrasYNumeros[] = ['a','b','c']    
+    let mostrarDatosNumero: letrasYNumeros[]  = [1,2,3]
+ 
+    console.log('Letras y Numeros', mostrarDatos,mostrarDatosNumero);
+}
+
+mostrarValores('Ejecutar funcion.....');
+
+
+
+
+
+
+
+
+
+
+
